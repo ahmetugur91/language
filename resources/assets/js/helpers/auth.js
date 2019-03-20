c1d2d9f570b3c1f84296beacce945e9b6f69b688
Ahmet UGUR <ahmet.ugur@std.gau.edu.tr>
2019-03-20 18:08:49 +0300

    update project

---
+++ resources/assets/js/helpers/auth.js
@@ -0,0 +1,38 @@
+import { setAuthorization } from "./general";
+
+export function login(credentials) {
+    return new Promise((res, rej) => {
+        axios.post('/api/auth/login', credentials)
+            .then((response) => {
+                setAuthorization(response.data.access_token);
+                res(response.data);
+            })
+            .catch((err) =>{
+                rej("Wrong email or password");
+            })
+    })
+}
+
+export function register(credentials) {
+    return new Promise((res, rej) => {
+        axios.post('/api/auth/register', credentials)
+            .then((response) => {
+                setAuthorization(response.data.access_token);
+                res(response.data);
+            })
+            .catch((err) =>{
+               // rej("Wrong email or password");
+                rej(err.response.data.errors);
+            })
+    })
+}
+
+export function getLocalUser() {
+    const userStr = localStorage.getItem("user");
+
+    if(!userStr) {
+        return null;
+    }
+
+    return JSON.parse(userStr);
+}
